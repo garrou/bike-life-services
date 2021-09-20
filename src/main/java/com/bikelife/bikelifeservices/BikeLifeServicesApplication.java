1b3eebb34e40fc70eee0dd38cf4e8b0fd356a89f
Adrien <39885692+1-irdA@users.noreply.github.com>
2021-09-20 10:28:03 +0200

    first commit

---
+++ src/main/java/com/bikelife/bikelifeservices/BikeLifeServicesApplication.java
@@ -0,0 +1,13 @@
+package com.bikelife.bikelifeservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeLifeServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeLifeServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bikelife/bikelifeservices/BikeLifeServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikelife.bikelifeservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeLifeServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
