50424869e4e7edff18f2542668ebcb7bc2f50348
Adrien <39885692+1-irdA@users.noreply.github.com>
2021-09-21 16:08:45 +0200

    'Update path'

--- src/main/java/com/bikelifeservices/bikelife/controllers/BikeController.java
+++ src/main/java/com/bikelifeservices/bikelife/controllers/BikeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path="api/v1/bikes")
+@RequestMapping(path = "api/v1/bikes")
 public class BikeController {
 
     private final BikeService bikeService;
