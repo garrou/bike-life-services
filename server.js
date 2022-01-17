b5063b1fd0b5de25ff0c4148f9e4bdf0dc211143
Adrien <39885692+1-irdA@users.noreply.github.com>
2022-01-17 17:33:26 +0100

    Add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = require('./controllers/routesController');
+const { cors } = require('./middlewares/cors');
 const app = express();
 
 app.use(express.json());
 
+app.use(cors);
 app.use(router);
 
 app.listen(8100, '::');
